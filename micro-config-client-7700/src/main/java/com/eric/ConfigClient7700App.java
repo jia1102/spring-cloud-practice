aa3b3e95acfea0e7307103b7017f3c4f79e0c7ea
liujiachuan <Ljch19851213!>
2018-08-30 09:56:47 +0800

    练习项目---spring_cloud---因为端口冲突，修改了Eureka7000d的端口，7000 改成 7003

--- micro-config-client-7700/src/main/java/com/eric/ConfigClient7700App.java
+++ micro-config-client-7700/src/main/java/com/eric/ConfigClient7700App.java
@@ -2,10 +2,10 @@ package com.eric;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.bootstrap.BootstrapConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@BootstrapConfiguration
+@EnableEurekaClient
 public class ConfigClient7700App {
     public static void main( String[] args ) {
         SpringApplication.run(ConfigClient7700App.class, args);
